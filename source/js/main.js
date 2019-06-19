dc27a92f57ac6f883d90db4cde11685b2a0fa783
Airet <thiefon@gmail.com>
2019-06-19 22:32:54 +0500

    адаптив для маленьких десктопов и планшета

--- source/js/main.js
+++ source/js/main.js
@@ -3,9 +3,26 @@
 
   // Запускаем плагин карусели
   carousel.owlCarousel({
-    items:4,
-    loop:true,
-    margin: 30
+    loop: true,
+    margin: 30,
+    responsive:{
+      0: {
+        items: 1
+      },
+
+      768: {
+        items: 2,
+        center: true
+      },
+
+      1025: {
+        items: 3
+      },
+
+      1351: {
+        items: 4
+      }
+    }
   });
 
   $(".services__btn--prev").click(function () {
